0e97890f15cd7cd04d5add140041c6567fd6a6ad
Aris Kurniawan <arisk.31337@gmail.com>
2017-03-26 16:22:55 +0700

    Update cert.rb

--- core/cert.rb
+++ core/cert.rb
@@ -5,10 +5,10 @@ class SharesploitCert
 		puts ""
 		puts "\e[1;31m"+"             The ShareSploit Framework (SSF)"+"\e[0m"
 		puts ""
-		puts " Creator   : Aris Kurniawan (akaig) - @0xffHack"
+		puts " Creator   : Aris Kurniawan"
 		puts " Version   : v0.1 alpha"
 		puts " Codename  : 'gollum'"
-		puts " Open issue: https://github.com/sharesec/sharesploit/issues/1"
+		puts " Open issue: https://github.com/kanopike/sharesploit/issues/1"
 		puts ""
 	end
 end
